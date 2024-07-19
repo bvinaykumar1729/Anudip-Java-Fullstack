30c1152316da039e1ca332f1f62b4d3816d67e7f
bvinaykumar1729 <bvinaykumar3663@gmail.com>
2024-07-19 18:54:23 +0530

    Third commit

---
+++ src/scannerDemo/InputDemo.java
@@ -0,0 +1,23 @@
+package scannerDemo;
+import java.util.*;
+public class InputDemo {
+
+	public static void main(String[] args) {
+		
+		        Scanner sc = new Scanner(System.in) ;
+
+		        System.out.println("Enter the age: ");
+		        int age =  sc.nextInt() ;
+
+		        System.out.println("Enter the name: ");
+		        String name = sc.nextLine() ;
+		        
+		        System.out.println("Age: " + age);
+		        System.out.println("Name: " + name);
+
+		    }
+		    }
+
+
+
+
