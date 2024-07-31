ec686d4cc19fe022f670656d3b1fb6e1470e0789
bvinaykumar1729 <bvinaykumar3663@gmail.com>
2024-07-31 23:23:55 +0530

    comments added

--- src/scannerDemo/InputDemo.java
+++ src/scannerDemo/InputDemo.java
@@ -3,15 +3,17 @@ import java.util.*;
 public class InputDemo {
 
 	public static void main(String[] args) {
-		
+		//creating object of Scanner class
 		        Scanner sc = new Scanner(System.in) ;
-
+		        
+		        //taking input through keyboard
 		        System.out.println("Enter the age: ");
 		        int age =  sc.nextInt() ;
 
 		        System.out.println("Enter the name: ");
 		        String name = sc.nextLine() ;
 		        
+		        //printing age and name 
 		        System.out.println("Age: " + age);
 		        System.out.println("Name: " + name);
 
