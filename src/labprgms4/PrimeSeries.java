512a6273b052870b0f43902cc15a08198ee5aeac
bvinaykumar1729 <bvinaykumar3663@gmail.com>
2024-08-05 00:48:25 +0530

    comments added

--- src/labprgms4/PrimeSeries.java
+++ src/labprgms4/PrimeSeries.java
@@ -3,22 +3,30 @@ import java.util.Scanner;
 
 //creating class for prime number
  class PrimeNumber{
-	 //creating member variables
+	 
+	 //creating member variables to store number
 	 int range,number,divisor;
+	 
+	 //--------------------------------------
 	 public void input() {
 		 //creating an object for Scanner class
 		 Scanner sc = new Scanner(System.in);
 		 System.out.println("Enter range of the number");
 		 range =sc.nextInt();	 
 	 }
+	 
+	 //--------------------------------------
+	 // method to generate prime number series
 	 public void primeNumberOperation() {
-		 System.out.print("Prime number series is :");
+		 
+		 System.out.print("Prime number series are :");
+		 // outer loop for certain range of numbers
 		 for(int i=1; i<=range; i++) {
 			 int divisor =0;
+			 // inner loop for to find divisors of that numbers
 			 for(int j=1; j<=i; j++) {
 				 if(i%j==0) {
-					 divisor++;
-					 
+					 divisor++; 
 				 }
 			 }
 			 if(divisor==2) {
@@ -28,11 +36,16 @@ import java.util.Scanner;
 		 }
 	 }
  }
+ //--------------------------------------
+ // main class 
 public class PrimeSeries {
 
 	public static void main(String[] args) {
+		// creating object for PrimeSeries class
 		PrimeNumber prime = new PrimeNumber();
+		// a method for input
 		prime.input();
+		// a method for prime number series
 		prime.primeNumberOperation();
 
 	}
